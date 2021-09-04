81ca4ea9c5741cab1b786bda5d1113f6c4a28e7a
YuraSergeevv <65806687+YuraSergeevv@users.noreply.github.com>
2021-09-04 17:09:03 +0300

    anim + padding + button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
 
+    $('.decoB_js').waypoint(function (dir) {
+        if (dir === 'down') {
+            jQuery(this).addClass('active');
+        }
+    }, {
+        offset: '65%'
+    });
 
 });
